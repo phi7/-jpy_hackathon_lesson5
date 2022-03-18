edf98dea9cfbf2c0cc6e084b3379759ad8e1f6ad
phi7 <42067723+phi7@users.noreply.github.com>
2022-03-18 20:36:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function myFunction() {
-  const provider = await new ethers.providers.Web3Provider(window.ehereum);
+  const provider = await new ethers.providers.Web3Provider(window.ethereum);
   const blocknumber = await provider.getBlockNumber();
   alert(blocknumber);
 } 
